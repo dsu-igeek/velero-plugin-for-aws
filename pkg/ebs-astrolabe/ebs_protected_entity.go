bb221d6d1ca47a975392ff4f2cf576daedb7fca6
Dave Smith-Uchida <dsmithuchida@vmware.com>
2021-06-03 08:17:37 -0700

    EBS PE basically working, data extraction works

--- pkg/ebs-astrolabe/ebs_protected_entity.go
+++ pkg/ebs-astrolabe/ebs_protected_entity.go
@@ -3,26 +3,64 @@ package ebs_astrolabe
 import (
 	"context"
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/ebs"
 	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/pkg/errors"
 	"github.com/vmware-tanzu/astrolabe/pkg/astrolabe"
+	"github.com/vmware-tanzu/astrolabe/pkg/util"
 	"io"
+	"sync"
 )
 
 type EBSProtectedEntity struct {
-	id        astrolabe.ProtectedEntityID
-	petm * EBSProtectedEntityTypeManager
+	id                  astrolabe.ProtectedEntityID
+	petm                *EBSProtectedEntityTypeManager
+	blockSize           *int
+	blockInfoCache      *map[int64]ebs.Block
+	blockInfoCacheMutex *sync.Mutex
 }
 
 func NewEBSProtectedEntity(id astrolabe.ProtectedEntityID, petm *EBSProtectedEntityTypeManager) EBSProtectedEntity {
+	var blockInfoCacheMutex sync.Mutex
+	blockInfoCache := make(map[int64]ebs.Block, 0)
+	blockSize := -1
 	return EBSProtectedEntity{
-		id:   id,
-		petm: petm,
+		id:                  id,
+		petm:                petm,
+		blockSize:           &blockSize,
+		blockInfoCache:      &blockInfoCache,
+		blockInfoCacheMutex: &blockInfoCacheMutex,
 	}
 }
 
 func (recv EBSProtectedEntity) GetInfo(ctx context.Context) (astrolabe.ProtectedEntityInfo, error) {
-	panic("implement me")
+	if recv.id.HasSnapshot() {
+		// TODO - fix GetInfoForSnapshot API to not return a pointer to ProtectedEntityInfo
+		snapshotInfo, err := recv.GetInfoForSnapshot(ctx, recv.id.GetSnapshotID())
+		// TODO - check for err and don't dereference if err
+		return *snapshotInfo, err
+	} else {
+		dvi := ec2.DescribeVolumesInput{
+			VolumeIds: []*string{aws.String(recv.id.GetID())},
+		}
+		dvo, err := recv.petm.ec2.DescribeVolumes(&dvi)
+		if err != nil {
+			return nil, errors.WithMessagef(err, "DescribeVolumes failed for EBS Protected Entity %s", recv.id.String())
+		}
+		name := ""
+		for _, checkTag := range dvo.Volumes[0].Tags {
+			if *checkTag.Key == "Name" {
+				name = *checkTag.Value
+			}
+		}
+		return astrolabe.NewProtectedEntityInfo(recv.id,
+			name,
+			(*dvo.Volumes[0].Size) * 1024 * 1024 * 1024,	// Convert GiB -> bytes
+			nil,
+			nil,
+			nil,
+			nil), nil
+	}
 }
 
 func (recv EBSProtectedEntity) GetCombinedInfo(ctx context.Context) ([]astrolabe.ProtectedEntityInfo, error) {
@@ -82,7 +120,7 @@ func (recv EBSProtectedEntity) Snapshot(ctx context.Context, params map[string]m
 		return astrolabe.ProtectedEntitySnapshotID{}, err
 	}
 
-	tags := make(map[string]string, 0)	// TODO - figure out what we want to do here and how to pass via Astrolabe or if necessary
+	tags := make(map[string]string, 0) // TODO - figure out what we want to do here and how to pass via Astrolabe or if necessary
 	snapshotTags := getTags(tags, volumeInfo.Tags)
 	snapshotInput := &ec2.CreateSnapshotInput{
 		VolumeId: &volumeID,
@@ -117,7 +155,7 @@ func (recv EBSProtectedEntity) ListSnapshots(ctx context.Context) ([]astrolabe.P
 
 	result, err := recv.petm.ec2.DescribeSnapshots(dsi)
 	if err != nil {
-		return nil, errors.WithMessagef(err,"Failed to list snapshots for %s", recv.id.String())
+		return nil, errors.WithMessagef(err, "Failed to list snapshots for %s", recv.id.String())
 	}
 	returnSnapshotIDs := make([]astrolabe.ProtectedEntitySnapshotID, len(result.Snapshots))
 	for curSnapshotNum, curSnapshot := range result.Snapshots {
@@ -131,25 +169,133 @@ func (recv EBSProtectedEntity) DeleteSnapshot(ctx context.Context, snapshotToDel
 }
 
 func (recv EBSProtectedEntity) GetInfoForSnapshot(ctx context.Context, snapshotID astrolabe.ProtectedEntitySnapshotID) (*astrolabe.ProtectedEntityInfo, error) {
-	panic("implement me")
+	dsi := ec2.DescribeSnapshotsInput{
+		SnapshotIds: []*string{aws.String(snapshotID.String())},
+	}
+	dvo, err := recv.petm.ec2.DescribeSnapshots(&dsi)
+	if err != nil {
+		return nil, errors.WithMessagef(err, "DescribeSnapshots failed for EBS Protected Entity %s", recv.id.String())
+	}
+	pei  := astrolabe.NewProtectedEntityInfo(recv.id.IDWithSnapshot(snapshotID),
+		*dvo.Snapshots[0].Description,
+		(*dvo.Snapshots[0].VolumeSize) * 1024 * 1024 * 1024,	// Convert GiB -> bytes
+		nil,
+		nil,
+		nil,
+		nil)
+	return &pei, nil
 }
 
 func (recv EBSProtectedEntity) GetComponents(ctx context.Context) ([]astrolabe.ProtectedEntity, error) {
-	panic("implement me")
+	return make([]astrolabe.ProtectedEntity, 0), nil
 }
 
 func (recv EBSProtectedEntity) GetID() astrolabe.ProtectedEntityID {
-	panic("implement me")
+	return recv.id
 }
 
 func (recv EBSProtectedEntity) GetDataReader(ctx context.Context) (io.ReadCloser, error) {
-	panic("implement me")
+	return util.NewReaderForBlockSource(recv, recv.petm.logger), nil
 }
 
 func (recv EBSProtectedEntity) GetMetadataReader(ctx context.Context) (io.ReadCloser, error) {
-	panic("implement me")
+	return nil, nil
 }
 
 func (recv EBSProtectedEntity) Overwrite(ctx context.Context, sourcePE astrolabe.ProtectedEntity, params map[string]map[string]interface{}, overwriteComponents bool) error {
 	panic("implement me")
+}
+
+func (recv EBSProtectedEntity) Read(startBlock uint64, numBlocks uint64, buffer []byte) error {
+	if !recv.id.HasSnapshot() {
+		return errors.Errorf("EBSProtectedEntity %s is not a snapshot", recv.id.String())
+	}
+	for curBlock:= startBlock; curBlock < startBlock + numBlocks; curBlock++ {
+		curBlockInt64 := int64(curBlock)
+		blockToken, err := recv.getBlockTokenForIndex(curBlockInt64)
+		if err != nil {
+			return err
+		}
+		gsbi := ebs.GetSnapshotBlockInput{
+			BlockIndex: &curBlockInt64,
+			BlockToken: blockToken,
+			SnapshotId: aws.String(recv.id.GetSnapshotID().String()),
+		}
+		gsbo, err := recv.petm.ebs.GetSnapshotBlock(&gsbi)
+		blockOffset := int(curBlock - startBlock)
+		bufOffset := blockOffset * *recv.blockSize
+		bytesRead, err := io.ReadFull(gsbo.BlockData, buffer[bufOffset:bufOffset + *recv.blockSize])
+		if bytesRead != *recv.blockSize {
+			return errors.Errorf("Expected %d bytes, got %d at block #", *recv.blockSize, bytesRead, curBlockInt64)
+		}
+		if err != nil {
+			return errors.WithMessagef(err,"Failed at block %d", curBlockInt64 )
+		}
+	}
+	return nil
+}
+
+func (recv EBSProtectedEntity) BlockSize() int {
+	recv.blockInfoCacheMutex.Lock()
+	defer recv.blockInfoCacheMutex.Unlock()
+	if *recv.blockSize < 0 {
+		recv.loadBlockInfoStartingAt(0)
+	}
+	return *recv.blockSize
+}
+
+func (recv EBSProtectedEntity) loadBlockInfoStartingAt(blockIndex int64) error {
+	maxResults := int64(1000)
+	lsbi := ebs.ListSnapshotBlocksInput{
+		MaxResults:         &maxResults,
+		NextToken:          nil,
+		SnapshotId:         aws.String(recv.id.GetSnapshotID().String()),
+		StartingBlockIndex: &blockIndex,
+	}
+	lsbo, err := recv.petm.ebs.ListSnapshotBlocks(&lsbi)
+	if err != nil {
+		return errors.WithMessagef(err, "Could not get block token for index %d for EBS Protected Entity %s", blockIndex, recv.id.String())
+	}
+	*recv.blockSize = int(*lsbo.BlockSize)
+	for _, curBlock := range lsbo.Blocks {
+		(*recv.blockInfoCache)[*curBlock.BlockIndex] = *curBlock
+	}
+	return nil
+}
+func (recv EBSProtectedEntity) getBlockTokenForIndex(blockIndex int64) (*string, error) {
+	if !recv.id.HasSnapshot() {
+		return nil, errors.Errorf("EBS Protected Entity %s is not a snapshot", recv.id.String())
+	}
+	recv.blockInfoCacheMutex.Lock()
+	defer recv.blockInfoCacheMutex.Unlock()
+	block, ok := (*recv.blockInfoCache)[blockIndex]
+	if !ok {
+		err := recv.loadBlockInfoStartingAt(blockIndex)
+		if err != nil {
+			return nil, err
+		}
+		block, ok = (*recv.blockInfoCache)[blockIndex]
+		if !ok {
+			return nil, errors.Errorf("block info not available for block %d in EBS Protected Entity %s",
+				blockIndex, recv.id.String())
+		}
+	}
+	return block.BlockToken, nil
+}
+
+func (recv EBSProtectedEntity) Capacity() int64 {
+	peInfo, err := recv.GetInfo(context.TODO())
+	if err != nil {
+		return 0
+	}
+	return peInfo.GetSize()
+}
+func (recv EBSProtectedEntity) Close() error {
+	recv.blockInfoCacheMutex.Lock()
+	defer recv.blockInfoCacheMutex.Unlock()
+	// Clear the block info cache on "closing" and don't leave it hanging around (could be big)
+	blockInfoCache := make(map[int64]ebs.Block, 0)
+	recv.blockInfoCache = &blockInfoCache
+	*recv.blockSize = -1
+	return nil
 }
